68f47e36673efaeabb98f6f1402d8d67e93afeaf
Juan Ignacio Muñoz <108429659+JuaniMunozOv@users.noreply.github.com>
2023-11-23 21:41:03 -0300

    webSenderismo

---
+++ webSenderismo/qgis2web_2023_11_23-21_32_44_857124/data/FinSenderismo_17.js
@@ -0,0 +1,17 @@
+var json_FinSenderismo_17 = {
+"type": "FeatureCollection",
+"name": "FinSenderismo_17",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.318796960853024, 38.721591417567559 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.181466072677208, 38.699582042398632 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.339773159670613, 38.688149130405378 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.267709109163783, 38.734483464583356 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.16082453867285, 38.715612884897624 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.194955278786496, 38.723517079105847 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.340782816788324, 38.790022884922543 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.111942403950208, 38.741070804862353 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.126561044856437, 38.772104149456048 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.104202461956522, 38.779181995000016 ] } }
+]
+}
---
+++ webSenderismo/qgis2web_2023_11_23-21_32_44_857124/data/IniciosSenderismo_18.js
@@ -0,0 +1,20 @@
+var json_IniciosSenderismo_18 = {
+"type": "FeatureCollection",
+"name": "IniciosSenderismo_18",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.315367070035919, 38.722102089414129 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.192509449118006, 38.678438227889444 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.16082973450169, 38.715637059121612 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.313848589907073, 38.723644175844619 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.267736298015134, 38.734533310810839 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.291066389887367, 38.748268190938269 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.114691390510949, 38.753854830656941 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.06692293768248, 38.744797765374116 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.340805597764601, 38.790034275410648 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.196561787034672, 38.71734520122277 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.111942649907589, 38.741072772521392 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.126561044856437, 38.772135632000804 ] } },
+{ "type": "Feature", "properties": { }, "geometry": { "type": "Point", "coordinates": [ -0.108472834782609, 38.778369582608697 ] } }
+]
+}
